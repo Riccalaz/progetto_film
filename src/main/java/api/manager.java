68126bf0bb5be09b4934305b91c7bd8a59e5162d
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2025-04-18 16:33:50 +0200

    Update manager.java

--- src/main/java/api/manager.java
+++ src/main/java/api/manager.java
@@ -1,15 +1,18 @@
 package api;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
-
 @Path("conn")
 public class manager {
-	DataBase db=new DataBase();
+	DataBase db = new DataBase();
+
 	@GET
 	public void connessione() {
 		try {
@@ -20,14 +23,62 @@ public class manager {
 			e.printStackTrace();
 		}
 	}
+	
 	@GET
-	@Path("{id}")
-	public Response get(@PathParam("id") long id) {
-		return null;
-        
-         //return Response.ok().build();
-         
-        
-        //return Response.status(Response.Status.NOT_FOUND).build();
-    }
+	@Path("ID/{id}")
+	public List<film> getDirector(@PathParam("id") int id) throws SQLException {
+		db.conn();
+		System.out.println("si");
+
+		List<film> F = db.ricercaInt("ID", id);
+		db.discon();
+
+		return F;
+	}
+
+	@GET
+	@Path("name/{id}")
+	public List<film> getName(@PathParam("id") String id) throws SQLException {
+		db.conn();
+		System.out.println("si");
+
+		List<film> F = db.ricercaString("name", id);
+		db.discon();
+
+		return F;
+	}
+
+	@GET
+	@Path("director/{id}")
+	public List<film> getDirector(@PathParam("id") String id) throws SQLException {
+		db.conn();
+		System.out.println("si");
+		List<film> F = db.ricercaString("director", id);
+		db.discon();
+
+		return F;
+	}
+	
+	@GET
+	@Path("rating")
+	public List<film> getRat() throws SQLException {
+		db.conn();
+		System.out.println("si");
+		List<film> F = db.ordinaRating();
+		db.discon();
+
+		return F;
+	}
+	
+	@POST
+	@Path("nline")
+	public Response add(film f) throws URISyntaxException, SQLException {
+		int newID;
+		
+		db.conn();
+		newID = db.insert(f);
+		db.discon();
+		return Response.created(new URI("api/conn/ID/" + newID)).build();
+		//senza dice solo di aver creato la risorsa
+	}
 }
